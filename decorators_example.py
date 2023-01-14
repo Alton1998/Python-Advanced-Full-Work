7817dc3da10b46363e24030f4e37c63c47204187
Alton D'souza <altondsouza02@gmail.com>
2023-01-14 22:15:55 +0530

    Fix

--- decorators_example.py
+++ decorators_example.py
@@ -33,4 +33,75 @@ def make_bold(func):
 def print_name():
     print('Alton')
 
+
 print_name()
+
+
+# Decorator with params
+# Notice this has a defined number of parameters
+
+def num_check(func):
+    def check_int(o):
+        if isinstance(0, int):
+            if o == 0:
+                print('Can not divide by zero')
+                return False
+            return True
+        print('f{o} is not a number')
+        return False
+
+    def inner(x, y):
+        if not check_int(x) or not check_int(y):
+            return
+        return func(x, y)
+
+    return inner
+
+
+@num_check
+def divide(a, b):
+    print(a / b)
+
+
+divide(100, 3)
+divide(0, 2)
+
+
+# divide(100, 'cat')
+
+
+# Decorator with unknown number of params
+# We want a decorator that can pass params and handle anything
+# We also want to chain them together
+
+# *args **kwargs to the rescue
+
+def outline(func):
+    def inner(*args, **kwargs):
+        print('-' * 20)
+        func(*args, **kwargs)
+        print(f'-'*20)
+    return inner
+
+
+def list_items(func):
+    def inner(*args, **kwargs):
+        func(*args, **kwargs)
+        print(f'args = {args}')
+        print(f'kwargs = {kwargs}')
+        for x in args:
+            print(f'arg={x}')
+        for k,v in kwargs.items():
+            print(f'key={k},value={v}')
+    return inner
+
+
+
+
+@outline  # this is run first
+@list_items
+def display(msg):
+    print(msg)
+
+
+display("hello")
